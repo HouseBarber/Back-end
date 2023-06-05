c7957bb0c372cd2e7c54379e95eb1f64c13ff885
JoileJr <joilejunior2@gmail.com>
2023-06-05 19:45:50 -0300

    Finalizando implementação de Email

--- src/main/java/com/projetoIntegradorII/HouseBarber/service/Utils/EmailServiceImpl.java
+++ src/main/java/com/projetoIntegradorII/HouseBarber/service/Utils/EmailServiceImpl.java
@@ -1,11 +1,32 @@
 package com.projetoIntegradorII.HouseBarber.service.Utils;
 
+import java.lang.module.Configuration;
+
+import javax.mail.MessagingException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
 public class EmailServiceImpl implements EmailService {
+    @Autowired
+    private JavaMailSenderImpl mailSender;
+
+    @Autowired
+    private Configuration fmConfiguration;
 
     @Override
-    public void sendSimpleMessage(String to, String subject, String text) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'sendSimpleMessage'");
+    public void sendSimpleMessage(String to, String subject, String text) throws MessagingException {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom("viniciusnakahara@gmail.com");
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(text);
+            mailSender.send(message);
+        } catch (Exception ex) {
+            throw new RuntimeException();
+        }
     }
     
 }
