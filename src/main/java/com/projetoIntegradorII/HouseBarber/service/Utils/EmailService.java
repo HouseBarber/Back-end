074eea4b4e62f0ea159bb115a4905e5c80de0a28
JoileJr <joilejunior2@gmail.com>
2023-06-05 19:45:50 -0300

    Finalizando implementação de Email

--- src/main/java/com/projetoIntegradorII/HouseBarber/service/Utils/EmailService.java
+++ src/main/java/com/projetoIntegradorII/HouseBarber/service/Utils/EmailService.java
@@ -1,6 +1,7 @@
 package com.projetoIntegradorII.HouseBarber.service.Utils;
 
+import javax.mail.MessagingException;
 
 public interface EmailService {
-    void sendSimpleMessage(String to, String subject, String text);
+    void sendSimpleMessage(String to, String subject, String text) throws MessagingException ;
 }
