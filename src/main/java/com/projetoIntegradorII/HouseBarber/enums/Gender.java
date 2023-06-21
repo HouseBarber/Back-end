925a595fb63448449f5221afca7022ebecc04742
JoileJr <joilejunior2@gmail.com>
2023-06-21 09:46:14 -0300

    tipos enumerados

--- src/main/java/com/projetoIntegradorII/HouseBarber/enums/Gender.java
+++ src/main/java/com/projetoIntegradorII/HouseBarber/enums/Gender.java
@@ -1,4 +1,33 @@
 package com.projetoIntegradorII.HouseBarber.enums;
 
 public enum Gender {
+    MASCULINO(0, "MASCULINO"), FEMININO(1, "FEMININO");
+	
+	private Integer codigo;
+	private String descricao;
+	
+	private Gender(Integer codigo, String descricao) {
+		this.codigo = codigo;
+        this.descricao = descricao;
+    }
+
+    public Integer getCodigo() {
+		return codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public static Gender toEnum(Integer cod) {
+		if(cod == null) {
+			return null;
+		}
+		for(Gender x : Gender.values() ) {
+			if(cod.equals(x.getCodigo())) {
+				return x;
+			}
+		}
+		throw new IllegalArgumentException("Genero invalido");
+	}
 }
