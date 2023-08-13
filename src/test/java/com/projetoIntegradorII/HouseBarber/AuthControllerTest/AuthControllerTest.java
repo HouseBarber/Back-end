600b75d34898aed53c1cd4b672a7194c9c822f30
Vinicius Koiti <viniciusnakahara@gmail.com>
2023-08-12 22:38:53 -0300

    #HB-103 - Correção de teste e padronização de scripts de inserts

--- src/test/java/com/projetoIntegradorII/HouseBarber/AuthControllerTest/AuthControllerTest.java
+++ src/test/java/com/projetoIntegradorII/HouseBarber/AuthControllerTest/AuthControllerTest.java
@@ -1,18 +1,25 @@
 package com.projetoIntegradorII.HouseBarber.AuthControllerTest;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.projetoIntegradorII.HouseBarber.dto.InfoDTO;
 import com.projetoIntegradorII.HouseBarber.dto.authentication.UserAuthDTO;
 import com.projetoIntegradorII.HouseBarber.entity.autenticathion.TokenRecovery;
 import com.projetoIntegradorII.HouseBarber.entity.autenticathion.UserAuth;
 import com.projetoIntegradorII.HouseBarber.repository.authentication.TokenRecoveryRepository;
 import com.projetoIntegradorII.HouseBarber.repository.authentication.UserAuthRepository;
+import org.apache.catalina.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.ResponseEntity;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,10 +32,10 @@ public class AuthControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
-    @MockBean
+    @Autowired
     private UserAuthRepository userAuthRepository;
 
-    @MockBean
+    @Autowired
     private TokenRecoveryRepository tokenRecoveryRepository;
 
 
@@ -40,11 +47,17 @@ public class AuthControllerTest {
     }
 
     @Test
-    public void isValidToken(){
+    public void isValidToken() throws JsonProcessingException {
         UserAuthDTO userAuthDTO = new UserAuthDTO();
         userAuthDTO.setEmail("viniciusnakahara@gmail.com");
-        ResponseEntity<UserAuthDTO> userAuthDTOEntity = restTemplate.postForEntity(getTestUrl(port) + "recoveryPassword", userAuthDTO, UserAuthDTO.class);
-        Optional<UserAuth> userAuthOptional = userAuthRepository.findByEmailEqualsIgnoreCase(userAuthDTOEntity.getBody().getEmail());
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(userAuthDTO);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> entity = new HttpEntity<>(json, headers);
+        ResponseEntity<InfoDTO<UserAuthDTO>> userAuthDTOEntity = restTemplate.exchange(getTestUrl(port) + "recoveryPassword", HttpMethod.POST,entity,
+                new ParameterizedTypeReference<InfoDTO<UserAuthDTO>>() {});
+        Optional<UserAuth> userAuthOptional = userAuthRepository.findByEmailEqualsIgnoreCase(userAuthDTOEntity.getBody().getObject().getEmail());
         if(userAuthOptional.isPresent()){
             Optional<TokenRecovery> tokenRecoveryOptional = tokenRecoveryRepository.findByToken(userAuthOptional.get().getTokenRecovery().getToken());
             assertThat(tokenRecoveryOptional.get().isValid());
@@ -54,16 +67,20 @@ public class AuthControllerTest {
     }
 
     @Test
-    public void getToken() {
+    public void getToken() throws JsonProcessingException {
         UserAuthDTO userAuthDTO = new UserAuthDTO();
         userAuthDTO.setEmail("viniciusnakahara@gmail.com");
-        ResponseEntity<UserAuthDTO> userAuthDTOEntity = restTemplate.postForEntity(getTestUrl(port) + "recoveryPassword", userAuthDTO, UserAuthDTO.class);
-        Optional<UserAuth> userAuthOptional = userAuthRepository.findByEmailEqualsIgnoreCase(userAuthDTOEntity.getBody().getEmail());
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(userAuthDTO);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> entity = new HttpEntity<>(json, headers);
+        ResponseEntity<InfoDTO<UserAuthDTO>> userAuthDTOEntity = restTemplate.exchange(getTestUrl(port) + "recoveryPassword", HttpMethod.POST,entity,
+                new ParameterizedTypeReference<InfoDTO<UserAuthDTO>>() {});
+        Optional<UserAuth> userAuthOptional = userAuthRepository.findByEmailEqualsIgnoreCase(userAuthDTOEntity.getBody().getObject().getEmail());
         if (userAuthOptional.isPresent()) {
             Optional<TokenRecovery> tokenRecoveryOptional = tokenRecoveryRepository.findByToken(userAuthOptional.get().getTokenRecovery().getToken());
             assertThat(tokenRecoveryOptional.isPresent());
         }
-
-
     }
 }
