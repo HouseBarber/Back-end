f1758f03983c0031dd05b71d71385ede66c74d0c
JoileJr <joilejunior2@gmail.com>
2023-09-12 22:37:41 -0300

    refatorando serviço

---
+++ src/main/java/com/projetoIntegradorII/HouseBarber/service/Utils/StringUtil.java
@@ -0,0 +1,8 @@
+package com.projetoIntegradorII.HouseBarber.service.Utils;
+
+public class StringUtil {
+    
+    public static boolean isNullOrEmpty(String txt){
+        return txt == null || txt.trim().isEmpty();
+    }
+}
--- src/main/java/com/projetoIntegradorII/HouseBarber/service/user/UserServiceImpl.java
+++ src/main/java/com/projetoIntegradorII/HouseBarber/service/user/UserServiceImpl.java
@@ -1,23 +1,22 @@
 package com.projetoIntegradorII.HouseBarber.service.user;
 
-import br.com.caelum.stella.validation.CNPJValidator;
-import br.com.caelum.stella.validation.CPFValidator;
-
-import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.projetoIntegradorII.HouseBarber.dto.InfoDTO;
 import com.projetoIntegradorII.HouseBarber.dto.address.AddressDTO;
+import com.projetoIntegradorII.HouseBarber.dto.authentication.RolesDTO;
 import com.projetoIntegradorII.HouseBarber.dto.authentication.UserAuthDTO;
 import com.projetoIntegradorII.HouseBarber.entity.address.Address;
 import com.projetoIntegradorII.HouseBarber.entity.autenticathion.UserAuth;
 import com.projetoIntegradorII.HouseBarber.exception.InfoException;
 import com.projetoIntegradorII.HouseBarber.repository.address.AddressRepository;
 import com.projetoIntegradorII.HouseBarber.repository.authentication.UserAuthRepository;
+import com.projetoIntegradorII.HouseBarber.service.Utils.StringUtil;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,8 +24,8 @@ import org.springframework.transaction.annotation.Transactional;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
-import java.util.function.Consumer;
 
 @Service
 @Transactional
@@ -38,8 +37,6 @@ public class UserServiceImpl implements UserService {
 
     private final AddressRepository addressRepository;
 
-    private final ObjectMapper objectMapper;
-
     @Override
     public InfoDTO<UserAuthDTO> update(Long id, UserAuthDTO userAuthDTO) {
         InfoDTO<UserAuthDTO> infoDTO = new InfoDTO<>();
@@ -48,7 +45,6 @@ public class UserServiceImpl implements UserService {
         
         if (userAuthOptional.isPresent()) {
             UserAuth userAuth = userAuthOptional.get();
-            mapUserAuthDTOToUserAuth(userAuthDTO, userAuth);
 
             AddressDTO addressDTO = userAuthDTO.getAddress();
             if (addressDTO != null) {
@@ -56,12 +52,12 @@ public class UserServiceImpl implements UserService {
                 if (address == null) {
                     address = new Address(); 
                 }
-                mapAddressDTOToAddress(addressDTO, address); 
+                updateAddress(addressDTO, address); 
                 addressRepository.save(address);
                 userAuth.setAddress(address);
             } 
 
-            userAuthRepository.saveAndFlush(userAuth);
+            userAuthRepository.save(userAuth);
 
             infoDTO.setMessage("Atualização realizada com sucesso");
             infoDTO.setStatus(HttpStatus.OK);
@@ -73,15 +69,28 @@ public class UserServiceImpl implements UserService {
         return infoDTO;
     }
 
-    private void mapAddressDTOToAddress(AddressDTO addressDTO, Address address) {
-        address.setId(addressDTO.getId());
-        address.setCep(addressDTO.getCep());
-        address.setState(addressDTO.getState());
-        address.setCity(addressDTO.getCity());
-        address.setStreet(addressDTO.getStreet());
-        address.setNeighborhood(addressDTO.getNeighborhood());
-        address.setComplement(addressDTO.getComplement());
-        address.setNumber(addressDTO.getNumber());
+    private void updateAddress(AddressDTO addressDTO, Address address) {
+        if(!StringUtil.isNullOrEmpty(addressDTO.getCep())){
+            address.setCep(addressDTO.getCep());
+        }
+        if(!StringUtil.isNullOrEmpty(addressDTO.getState())){
+            address.setState(addressDTO.getState());
+        }
+        if(!StringUtil.isNullOrEmpty(addressDTO.getCity())){
+            address.setCity(addressDTO.getCity());
+        }
+        if(!StringUtil.isNullOrEmpty(addressDTO.getStreet())){
+            address.setStreet(addressDTO.getStreet());
+        }      
+        if(!StringUtil.isNullOrEmpty(addressDTO.getNeighborhood())){
+            address.setNeighborhood(addressDTO.getNeighborhood());
+        }
+        if(!StringUtil.isNullOrEmpty(addressDTO.getComplement())){
+            address.setComplement(addressDTO.getComplement());
+        }
+        if(!StringUtil.isNullOrEmpty(addressDTO.getNumber())){
+            address.setNumber(addressDTO.getNumber());
+        }
     }
 
     @Override
@@ -106,46 +115,4 @@ public class UserServiceImpl implements UserService {
         }
     }
 
-    private void mapUserAuthDTOToUserAuth(UserAuthDTO userAuthDTO, UserAuth userAuth) {
-        JsonNode userAuthNode = objectMapper.valueToTree(userAuthDTO);
-    
-        if (userAuthNode.has("name")) {
-            userAuth.setName(userAuthNode.get("name").asText());
-        }
-        if (userAuthNode.has("email")) {
-            userAuth.setEmail(userAuthNode.get("email").asText());
-        }
-        if (userAuthNode.has("gender")) {
-            userAuth.setGender(userAuthNode.get("gender").asText());
-        }
-        if (userAuthNode.has("username")) {
-            userAuth.setUsername(userAuthNode.get("username").asText());
-        }
-        if (userAuthNode.has("telephone")) {
-            userAuth.setTelephone(userAuthNode.get("telephone").asText());
-        }
-        if (userAuthNode.has("description")) {
-            userAuth.setDescription(userAuthNode.get("description").asText());
-        }
-        if (userAuthNode.has("cpf")) {
-            userAuth.setCpf(userAuthNode.get("cpf").asText());
-        }
-        if (userAuthNode.has("cnpj")) {
-            userAuth.setCnpj(userAuthNode.get("cnpj").asText());
-        }
-        if (userAuthNode.has("dateBirth")) {
-            String dateBirthStr = userAuthNode.get("dateBirth").asText();
-            Date dateBirth = parseDate(dateBirthStr);
-            userAuth.setDateBirth(dateBirth);
-        }   
-    }
-
-    private Date parseDate(String dateStr) {
-        try {
-            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-            return dateFormat.parse(dateStr);
-        } catch (ParseException e) {
-            return null;
-        }
-    }
 }
