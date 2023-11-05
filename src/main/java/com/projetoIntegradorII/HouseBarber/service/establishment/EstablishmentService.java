da6ff4269b55663867689c4f350e59d3e505f8d5
Vinicius Koiti <viniciusnakahara@gmail.com>
2023-11-05 19:10:32 -0300

    Busca establishment por id

--- src/main/java/com/projetoIntegradorII/HouseBarber/service/establishment/EstablishmentService.java
+++ src/main/java/com/projetoIntegradorII/HouseBarber/service/establishment/EstablishmentService.java
@@ -2,11 +2,9 @@ package com.projetoIntegradorII.HouseBarber.service.establishment;
 
 import com.projetoIntegradorII.HouseBarber.dto.InfoDTO;
 import com.projetoIntegradorII.HouseBarber.dto.establishment.EstablishmentDTO;
-import com.projetoIntegradorII.HouseBarber.entity.establishment.Establishment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 
 public interface EstablishmentService {
      InfoDTO<EstablishmentDTO> creatEstablishment(EstablishmentDTO establishmentDTO);
